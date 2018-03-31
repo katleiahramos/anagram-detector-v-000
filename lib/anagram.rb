60f6a5216b5c1abfed788d2cfea309604978ddc8
Katleiah Ramos <katleiahramos@gmail.com>
2018-03-31 17:31:11 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  @@anagrams = []
+
+  def initialize(word)
+    @word = word
+  end
+
+
+  def self.match(words)
+
+
+
+
+  end
+
+
+
+
+
+
+
+
+
+end
